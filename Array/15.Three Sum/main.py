c0c427263815c8d90102ba92fd5486abdefb9e52
Minghe Zhang <Minghe0Zhang@users.noreply.github.com>
2020-10-07 20:31:45 -0400

    finish 15

--- Array/15.Three Sum/main.py
+++ Array/15.Three Sum/main.py
@@ -12,4 +12,37 @@ Input: nums = [-1,0,1,2,-1,-4]
 Output: [[-1,-1,2],[-1,0,1]]
 """
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums):
+        nums.sort()
+        res = []
+        len_arr = len(nums)
+        a   = 0
+        for a in range(len_arr -2):
+            if (a>0 and nums[a]==nums[a-1]):
+                continue
+            left  = a + 1
+            right = len_arr - 1
+
+            while(left < right):
+                if(nums[left]+nums[right]+nums[a] < 0):
+                    left = left + 1
+                elif(nums[left]+nums[right]+nums[a] > 0):
+                    right = right - 1
+                else:
+                    res.append([nums[a],nums[left],nums[right]])
+                    while(left<right and nums[left+1]==nums[left]):
+                        left = left + 1
+                    while(left<right and nums[right-1]==nums[right]):
+                        right = right - 1
+                    right = right - 1
+                    left = left + 1
+        return res
+
+
+
+
+if __name__ == "__main__":
+    test = [-1,0,1,2,-1,-4]
+    sol = Solution()
+    res = sol.threeSum(test)
+    print(res)
