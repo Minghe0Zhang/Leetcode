c189f8fb2211520cf92ca254202082bb1751abf8
Minghe Zhang <Minghe0Zhang@users.noreply.github.com>
2020-10-24 11:29:02 -0400

    24 finished

---
+++ Linked-List/24. Swap Nodes in Pairs/main.py
@@ -0,0 +1,31 @@
+"""
+Merge two sorted linked lists and return it as a new sorted list. The new list should be made by 
+splicing together the nodes of the first two lists.
+
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        ans = ListNode(1)
+        cur = ans
+        if not head or not head.next:
+            return head
+        while head and head.next:
+            slow, fast = head, head.next
+            head       = fast.next
+            fast.next  = slow
+            slow.next  = head
+            cur.next   = fast
+            cur        = cur.next.next
+            
+        return ans.next
