4fa1df518d03a628342a0b1366d1f3f4119826b4
Minghe Zhang <Minghe0Zhang@users.noreply.github.com>
2020-10-26 14:49:02 -0400

    104 finished

---
+++ Tree/104. Maximum Depth of Binary Tree/README.md
@@ -0,0 +1,2 @@
+# 104. Maximum Depth of Binary Tree
+迭代/递归
---
+++ Tree/104. Maximum Depth of Binary Tree/main.py
@@ -0,0 +1,48 @@
+"""
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest
+leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        # if root == None:
+        #     return 0
+        # else:
+        #     return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
+        
+        if not root:
+            return 0
+        queue = [(root,1)]
+        max_depth = 1
+        while(queue):
+            node,depth = queue.pop(0)
+            if depth > max_depth:
+                max_depth = depth
+            if node.left:
+                queue.append([node.left, depth+1])
+            if node.right:
+                queue.append([node.right, depth+1])
+        return max_depth
